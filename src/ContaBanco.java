41b72fbea781120492ff07ed4d71b24f937d4ae6
gustavo-r-nunes <gustavo.r.nunes97@gmail.com>
2024-05-23 19:55:28 -0300

    finalizando

---
+++ src/ContaBanco.java
@@ -0,0 +1,41 @@
+public class ContaBanco {
+
+    private int numero;
+
+    private String agencia;
+    private String nomeCliente;
+
+    private float saldo;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+}
--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,27 @@
-public class Main {
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ContaBanco contaBanco = new ContaBanco();
+
+        System.out.println("Digite o número da conta: ");
+        contaBanco.setNumero(scanner.nextInt());
+
+        System.out.println("Digite o número da agência: ");
+        contaBanco.setAgencia(scanner.next());
+
+        System.out.println("Digite o nome do cliente: ");
+        contaBanco.setNomeCliente(scanner.next());
+
+        System.out.println("Digite o número do saldo: ");
+        contaBanco.setSaldo(scanner.nextInt());
+
+        System.out.println("Olá, " + contaBanco.getNomeCliente() +
+                ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + contaBanco.getAgencia()
+                + " , conta " + contaBanco.getNumero() +
+                " e seu saldo " + contaBanco.getSaldo() + " já está disponível para saque");
+
     }
 }
